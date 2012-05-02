b270d2931867013118468dfc5736a492a876541f
Katrina Owen <katrina.owen@gmail.com>
2012-05-02 22:30:38 +0200

    Integrate munger and loader

--- lib/langulator.rb
+++ lib/langulator.rb
@@ -1,3 +1,18 @@
+require 'langulator/loader'
+require 'langulator/munger'
+
 module Langulator
-  # Your code goes here...
+  def self.compile(options = {})
+    loader = Loader.new(options)
+    munger = Munger.new(:language => loader.origin, :translations => loader.source_translations, :alternates => loader.destination_translations)
+
+    munger.munge
+  end
+
+  def self.write(options)
+    translations = compile(options)
+    File.open(options[:to], 'w') do |file|
+      file.write translations.to_yaml
+    end
+  end
 end
---
+++ spec/langulator_spec.rb
@@ -0,0 +1,33 @@
+# encoding: utf-8
+require 'langulator'
+
+describe Langulator do
+  let(:options) { {:base_path => 'spec/fixtures/**/', :origin => 'english', :alternates => ['norsk', 'francais']} }
+
+  let(:combined) do
+    {
+      "spec/fixtures/" => {
+        "food" => {
+          "breakfast" => {"english" => "yoghurt", "norsk" => nil, "francais" => nil},
+          "lunch" => {"english" => "sandwich", "norsk" => "smørbrød", "francais" => nil},
+          "dinner" => {
+            "main_course" => {"english" => "steak", "norsk" => nil, "francais" => nil},
+            "side" => {"english" => "baked potato", "norsk" => nil, "francais" => nil},
+            "desert" => {"english" => "chocolate mousse", "norsk" => nil, "francais" => nil}
+          }
+        }
+      },
+      "spec/fixtures/lang/" => {
+        "volume" => {
+          "sound" => {"english" => "loud", "norsk" => nil, "francais" => nil},
+          "liquid" => {"english" => "sloshing", "norsk" => nil, "francais" => nil},
+          "hair" => {"english" => "because I'm worth it", "norsk" => nil, "francais" => nil}
+        }
+      }
+    }
+  end
+
+  it "loads and munges" do
+    Langulator.compile(options).should eq(combined)
+  end
+end
