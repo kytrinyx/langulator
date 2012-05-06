61f6c7906206ac3cc79817a2c746b68ce4d232d1
Katrina Owen <katrina.owen@gmail.com>
2012-05-06 20:47:32 +0200

    Delete unused require

--- lib/langulator.rb
+++ lib/langulator.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 require 'yaml'
-require 'langulator/aggregate'
 require 'langulator/translation'
 require 'langulator/aggregate_translation'
 require 'langulator/individual_translation'
